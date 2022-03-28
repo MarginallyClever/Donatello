7617583c1c50eb280f02f364438da66bbd96e5cd
Dan Royer <dan@marginallyclever.com>
2022-03-28 10:39:11 -0700

    added test

--- src/test/java/com/marginallyclever/donatello/ClassLoadingTest.java
+++ src/test/java/com/marginallyclever/donatello/ClassLoadingTest.java
@@ -2,7 +2,17 @@ package com.marginallyclever.donatello;
 
 import com.marginallyclever.nodegraphcore.*;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.Scanner;
 
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.FileSystems;
 import java.util.ServiceLoader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -49,6 +59,19 @@ public class ClassLoadingTest {
             assertEquals(addLoader, classLoader);
         } else {
             System.out.println("Did not thoroughly run this test");
+        }
+        NodeFactory.clear();
+    }
+
+    @Test
+    public void testLoadingDonatelloExtensionsIfAvailable() throws Exception {
+        System.out.println("Loading Donatello extensions: "+FileHelper.getExtensionPath());
+
+        ServiceLoaderHelper.addPathFiles(FileHelper.getExtensionPath());
+
+        NodeFactory.loadRegistries();
+        for(String name : NodeFactory.getNames()) {
+            System.out.println(name);
         }
         NodeFactory.clear();
     }
