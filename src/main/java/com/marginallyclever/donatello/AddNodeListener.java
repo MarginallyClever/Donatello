f000178a145addaec3a367677cc21964fcf0fc7c
Dan Royer <dan@marginallyclever.com>
2025-01-30 12:45:50 -0800

    Create AddNodeListener.java
    for decoupling

---
+++ src/main/java/com/marginallyclever/donatello/AddNodeListener.java
@@ -0,0 +1,12 @@
+package com.marginallyclever.donatello;
+
+import com.marginallyclever.nodegraphcore.Node;
+
+import java.util.EventListener;
+
+/**
+ * For listening to the {@link AddNodePanel}.
+ */
+public interface AddNodeListener extends EventListener {
+    void addNode(Node node);
+}
