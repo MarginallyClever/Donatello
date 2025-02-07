11d4ab8d0e3b8ea9f353f940729e168e8fd11fdd
Dan Royer <dan@marginallyclever.com>
2025-02-07 10:54:52 -0800

    update Port rectangle here instead of in NodeGraphCore

--- src/main/java/com/marginallyclever/donatello/ports/InputImage.java
+++ src/main/java/com/marginallyclever/donatello/ports/InputImage.java
@@ -2,6 +2,7 @@ package com.marginallyclever.donatello.ports;
 
 import com.marginallyclever.nodegraphcore.port.Input;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 /**
@@ -10,5 +11,17 @@ import java.awt.image.BufferedImage;
 public class InputImage extends Input<BufferedImage> {
     public InputImage(String name) throws IllegalArgumentException {
         super(name, BufferedImage.class, new BufferedImage(1,1,BufferedImage.TYPE_INT_ARGB));
+    }
+
+    @Override
+    public Rectangle getRectangle() {
+        double w = value.getWidth();
+        double h = value.getHeight();
+        if(w<rectangle.width && h<DEFAULT_HEIGHT) return rectangle;
+        double ratio = h/w;
+        var newh = (double)rectangle.width * ratio;
+        rectangle.setSize(rectangle.width,(int)Math.max(newh,DEFAULT_HEIGHT));
+
+        return super.getRectangle();
     }
 }
--- src/main/java/com/marginallyclever/donatello/ports/OutputImage.java
+++ src/main/java/com/marginallyclever/donatello/ports/OutputImage.java
@@ -2,10 +2,23 @@ package com.marginallyclever.donatello.ports;
 
 import com.marginallyclever.nodegraphcore.port.Output;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class OutputImage extends Output<BufferedImage> {
     public OutputImage(String _name) throws IllegalArgumentException {
         super(_name, BufferedImage.class, new BufferedImage(1,1,BufferedImage.TYPE_INT_ARGB));
+    }
+
+    @Override
+    public Rectangle getRectangle() {
+        double w = value.getWidth();
+        double h = value.getHeight();
+        if(w<rectangle.width && h<DEFAULT_HEIGHT) return rectangle;
+        double ratio = h/w;
+        var newh = (double)rectangle.width * ratio;
+        rectangle.setSize(rectangle.width,(int)Math.max(newh,DEFAULT_HEIGHT));
+
+        return super.getRectangle();
     }
 }
