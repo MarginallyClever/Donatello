03d2b83eea566d792da1065703d7b58d0f44de70
Dan Royer <dan@marginallyclever.com>
2025-02-09 17:02:46 -0800

    added constructor for default value 0

--- src/main/java/com/marginallyclever/donatello/ports/InputDouble.java
+++ src/main/java/com/marginallyclever/donatello/ports/InputDouble.java
@@ -9,6 +9,10 @@ import java.awt.*;
 public class InputDouble extends Input<Double> implements SwingProvider {
     private SelectDouble selectDouble;
 
+    public InputDouble(String name) throws IllegalArgumentException {
+        super(name, Double.class, 0.0);
+    }
+
     public InputDouble(String name, Double startingValue) throws IllegalArgumentException {
         super(name, Double.class, startingValue);
     }
