6db97a3ce28ac76b53cd24ec2ae1f56d1c85468e
Dan Royer <dan@marginallyclever.com>
2025-02-19 10:19:51 -0800

    Create InputRange.java

---
+++ src/main/java/com/marginallyclever/donatello/ports/InputRange.java
@@ -0,0 +1,58 @@
+package com.marginallyclever.donatello.ports;
+
+import com.marginallyclever.donatello.SwingProvider;
+import com.marginallyclever.donatello.select.Select;
+import com.marginallyclever.donatello.select.SelectSlider;
+import com.marginallyclever.nodegraphcore.port.Input;
+
+import java.awt.*;
+
+public class InputRange extends Input<Integer> implements SwingProvider {
+    private SelectSlider selectSlider;
+    private int top, bottom;
+
+    public InputRange(String name) {
+        this(name,0);
+    }
+
+    public InputRange(String name, Integer startingValue) throws IllegalArgumentException {
+        this(name, startingValue,100,0);
+    }
+
+    public InputRange(String name, Integer startingValue, int top, int bottom) throws IllegalArgumentException {
+        super(name, Integer.class, startingValue);
+        this.top = top;
+        this.bottom = bottom;
+    }
+
+    @Override
+    public Select getSwingComponent(Component parent) {
+        if(selectSlider==null) {
+            selectSlider = new SelectSlider(name,name,top,bottom,value);
+            selectSlider.addSelectListener( evt -> setValue(evt.getNewValue()) );
+        }
+        return selectSlider;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+        if(selectSlider!=null) {
+            selectSlider.setTop(top);
+        }
+    }
+
+    public void setBottom(int bottom) {
+        this.bottom = bottom;
+        if(selectSlider!=null) {
+            selectSlider.setBottom(bottom);
+        }
+    }
+}
