d9ad15d97f4236a8d7bd093a3305cfba05b3e794
Dan Royer <dan@marginallyclever.com>
2025-05-27 14:38:04 -0700

    added reset button to each line

--- src/main/java/com/marginallyclever/donatello/actions/ActionRegistry.java
+++ src/main/java/com/marginallyclever/donatello/actions/ActionRegistry.java
@@ -2,18 +2,36 @@ package com.marginallyclever.donatello.actions;
 
 import javax.swing.*;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Coordinates the actions available in the application.
  */
 public class ActionRegistry {
     private static final Map<String, Action> actions = new LinkedHashMap<>();
+    private static final Map<String, KeyStroke> defaultAccelerators = new HashMap<>();
 
+    /**
+     * Registers an action with the given id.
+     * @param id The unique identifier for the action.
+     * @param action The action to register.
+     * @throws IllegalArgumentException if an action with the same id is already registered.
+     */
     public static void register(String id, Action action) {
         actions.put(id, action);
+        defaultAccelerators.put(id, (KeyStroke) action.getValue(Action.ACCELERATOR_KEY));
     }
 
+    /**
+     * Gets the action with the given id.
+     * @param id The unique identifier for the action.
+     * @return The action associated with the given id.
+     * @throws IllegalArgumentException if no action with the given id is registered.
+     */
     public static Action get(String id) {
+        if(!actions.containsKey(id)) {
+            throw new IllegalArgumentException("Action with id '" + id + "' is not registered.");
+        }
         return actions.get(id);
     }
 
@@ -23,5 +41,28 @@ public class ActionRegistry {
 
     public static Set<Map.Entry<String, Action>> entrySet() {
         return actions.entrySet();
+    }
+
+    /**
+     * Returns the default accelerator for the action with the given id.
+     * @param id The id of the action.
+     * @return The default KeyStroke for the action.
+     * @throws IllegalArgumentException if no default accelerator is registered for the action.
+     */
+    public static KeyStroke getDefaultAccelerator(Action id) {
+        KeyStroke ks = (KeyStroke)id.getValue(Action.ACCELERATOR_KEY);
+        if(ks == null) return null;
+        AtomicReference<String> key = new AtomicReference<>("");
+
+        defaultAccelerators.forEach((k, v) -> {
+            if(ks.equals(v)) {
+                key.set(k);
+            }
+        });
+        String value = key.get();
+        if(value.isEmpty()) {
+            throw new IllegalArgumentException("No default accelerator for action with id '" + id + "'.");
+        }
+        return defaultAccelerators.get(value);
     }
 }
--- src/main/java/com/marginallyclever/donatello/actions/ActionSettingsPanel.java
+++ src/main/java/com/marginallyclever/donatello/actions/ActionSettingsPanel.java
@@ -8,69 +8,113 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * A panel that displays the settings for each Action in the application.
  */
 public class ActionSettingsPanel extends JPanel {
+    private final ArrayList<Action> list = ActionRegistry.getAll();
+
     public ActionSettingsPanel() {
         super();
-        setLayout(new GridLayout(0,2));
+        setLayout(new GridBagLayout());
 
-        var list = ActionRegistry.getAll();
+        var c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weighty = 0.0;
+        c.anchor = GridBagConstraints.WEST;
+
         for( var action : list ) {
-            var name = (String)action.getValue(Action.NAME);
-            var icon = (Icon)action.getValue(Action.SMALL_ICON);
-            // if icon is null, use a blank icon
-            if (icon == null) {
-                icon = new ImageIcon(new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB));
+            addLineItem(action,c);
+            c.gridy++;
+        }
+
+        checkForCollisions(list);
+    }
+
+    private void addLineItem(Action action,GridBagConstraints c) {
+        var name = (String)action.getValue(Action.NAME);
+
+        var icon = (Icon)action.getValue(Action.SMALL_ICON);
+        // if icon is null, use a blank icon
+        if (icon == null) {
+            icon = new ImageIcon(new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB));
+        }
+
+        // get the accelerator key for the action.
+        KeyStroke ks = (KeyStroke) action.getValue(Action.ACCELERATOR_KEY);
+        String readable = ks != null ? toHumanReadable(ks) : "";
+        var field = new JTextField(readable);
+
+        KeyStroke defaultKs = ActionRegistry.getDefaultAccelerator(action);
+        JButton resetButton = new JButton("");
+        // use icons8-reset-16.png as the icon for the reset button.
+        resetButton.setIcon( new ImageIcon(Objects.requireNonNull(getClass().getResource("/com/marginallyclever/donatello/icons8-reset-16.png"))));
+        updateResetButton(resetButton, defaultKs, ks);
+        resetButton.addActionListener(e->{
+            // reset the action's accelerator key to the default
+            action.putValue(Action.ACCELERATOR_KEY, defaultKs);
+            field.setText(toHumanReadable(defaultKs));
+            field.setForeground(Color.BLACK); // reset the color to default
+            resetButton.setEnabled(false);
+            checkForCollisions(list);
+        });
+
+        // put it together
+        c.gridx=0;
+        c.weightx=0.5;
+        this.add(new JLabel(name, icon, SwingConstants.LEFT),c);
+        c.gridx++;
+        this.add(field,c);
+        c.weightx=0.0;
+        c.gridx++;
+        this.add(resetButton,c);
+
+        // in a perfect world, catch any key combination typed in the field that is a valid accelerator and
+        // record that as the new accelerator key for the action.
+        // in reality, let the user type in the field and then validate it.
+        field.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                validate();
             }
 
-            KeyStroke ks = (KeyStroke) action.getValue(Action.ACCELERATOR_KEY);
-            String readable = ks != null ? toHumanReadable(ks) : "";
-            this.add(new JLabel(name, icon, SwingConstants.LEFT));
-            var field = new JTextField(readable);
-            this.add(field);
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                validate();
+            }
 
-            // catch any key combination entered in the field that is a valid accelerator
-            // record that as the new accelerator key for the action.
-            field.getDocument().addDocumentListener(new DocumentListener() {
-                @Override
-                public void insertUpdate(DocumentEvent e) {
-                    validate();
-                }
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                validate();
+            }
 
-                @Override
-                public void removeUpdate(DocumentEvent e) {
-                    validate();
-                }
-
-                @Override
-                public void changedUpdate(DocumentEvent e) {
-                    validate();
-                }
-
-                void validate() {
-                    String text = field.getText();
-                    if (text == null || text.trim().isEmpty()) {
-                        action.putValue(Action.ACCELERATOR_KEY, null);
-                        checkForCollisions(list);
-                        return;
-                    }
-                    KeyStroke newKeyStroke = KeyStroke.getKeyStroke(text);
-                    // invalid key stroke
-                    if(newKeyStroke == null) {
-                        field.setForeground(Color.RED);
-                        return;
-                    }
-                    // valid key stroke.
-                    action.putValue(Action.ACCELERATOR_KEY, newKeyStroke);
+            void validate() {
+                String text = field.getText();
+                if (text == null || text.trim().isEmpty()) {
+                    action.putValue(Action.ACCELERATOR_KEY, null);
                     checkForCollisions(list);
+                    return;
                 }
-            });
+                KeyStroke newKeyStroke = KeyStroke.getKeyStroke(text);
+                // invalid key stroke
+                if(newKeyStroke == null) {
+                    field.setForeground(Color.RED);
+                    return;
+                }
+                // valid key stroke.
+                action.putValue(Action.ACCELERATOR_KEY, newKeyStroke);
+                updateResetButton(resetButton, defaultKs, newKeyStroke);
+                checkForCollisions(list);
+            }
+        });
+    }
 
-            checkForCollisions(list);
-        }
+    private void updateResetButton(JButton resetButton, KeyStroke defaultKs, KeyStroke ks) {
+        resetButton.setEnabled( defaultKs != null && !defaultKs.equals(ks) );
     }
 
     private void checkForCollisions(ArrayList<Action> list) {
@@ -96,12 +140,10 @@ public class ActionSettingsPanel extends JPanel {
                 var action2 = list.get(j);
                 var ks2 = (KeyStroke) action2.getValue(Action.ACCELERATOR_KEY);
                 String text2 = ks2 != null ? toHumanReadable(ks2) : "";
-
                 if(text2==null || text2.trim().isEmpty()) continue;
-                // check if the text matches the other action's accelerator key
+
+                // if two actions have the same accelerator key, mark them as a collision.
                 if( text1.equals(text2) ) {
-                    // match!
-                    System.out.println("Collision found: " + text1 + " and " + text2);
                     // set the fields to red
                     for (Component comp : getComponents()) {
                         if (comp instanceof JTextField f2 && f2.getText().equals(text1)) {
@@ -113,6 +155,12 @@ public class ActionSettingsPanel extends JPanel {
         }
     }
 
+    /**
+     * Convert a {@link KeyStroke} to a human readable string that matches the format used in
+     * {@link KeyStroke#getKeyStroke(String)}
+     * @param ks the KeyStroke to convert
+     * @return a human readable string representation of the {@link KeyStroke}
+     */
     public String toHumanReadable(KeyStroke ks) {
         if (ks == null) return "";
         StringBuilder sb = new StringBuilder();
