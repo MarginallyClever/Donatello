bd376717d82115ccd1dff4d332ae44e7853dcd45
Dan Royer <dan@marginallyclever.com>
2025-07-18 21:18:45 -0700

    Draw larger icons in the add node tree

--- src/main/java/com/marginallyclever/donatello/FactoryCategoryCellRenderer.java
+++ src/main/java/com/marginallyclever/donatello/FactoryCategoryCellRenderer.java
@@ -7,12 +7,15 @@ import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Custom cell renderer for displaying node categories in a tree.
  * It sets the text and color based on the category's supplier.
  */
 class FactoryCategoryCellRenderer extends DefaultTreeCellRenderer {
+    private static final int ICON_SIZE = 24;
+
     @Override
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
         JLabel label = (JLabel) super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
@@ -27,20 +30,37 @@ class FactoryCategoryCellRenderer extends DefaultTreeCellRenderer {
                     if (n != null) {
                         var icon = n.getIcon();
                         if(icon != null) {
-                            label.setIcon(icon);
+                            label.setIcon(scaleIcon(icon,ICON_SIZE,ICON_SIZE));
                         } else {
-                            label.setIcon(getDefaultLeafIcon());
+                            label.setIcon(scaleIcon(getDefaultLeafIcon(),ICON_SIZE,ICON_SIZE));
                         }
                     } else {
-                        label.setIcon(getDefaultLeafIcon());
+                        label.setIcon(scaleIcon(getDefaultLeafIcon(),ICON_SIZE,ICON_SIZE));
                     }
                 } catch (Exception e) {
-                    label.setIcon(getDefaultLeafIcon());
+                    label.setIcon(scaleIcon(getDefaultLeafIcon(),ICON_SIZE,ICON_SIZE));
                 }
             } else {
-                label.setIcon(getDefaultOpenIcon());
+                label.setIcon(scaleIcon(getDefaultOpenIcon(),ICON_SIZE,ICON_SIZE));
             }
         }
         return label;
+    }
+
+    private Icon scaleIcon(Icon icon, int width, int height) {
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = img.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+        // Paint the icon into a temp image at its original size
+        BufferedImage iconImage = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D gIcon = iconImage.createGraphics();
+        icon.paintIcon(null, gIcon, 0, 0);
+        gIcon.dispose();
+
+        // Draw the temp image scaled to the target size
+        g2.drawImage(iconImage, 0, 0, width, height, null);
+        g2.dispose();
+        return new ImageIcon(img);
     }
 }
